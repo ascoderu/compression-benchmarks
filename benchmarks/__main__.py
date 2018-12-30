5f032405c7aca17b9749757cb3185aad786f2015
Clemens Wolff <clewolff@microsoft.com>
2018-12-29 23:44:32 -0500

    Switch to streaming console output

--- benchmarks/__main__.py
+++ benchmarks/__main__.py
@@ -3,9 +3,6 @@ from glob import glob
 from os import makedirs
 from os.path import join
 
-from colorama import Fore, Style
-from tablib import Dataset
-
 from benchmarks.utils import filesize, load_sample_email, timer, get_strategies, download_sample_emails
 
 parser = ArgumentParser()
@@ -19,14 +16,10 @@ makedirs(args.results_dir, exist_ok=True)
 
 sample_emails = [load_sample_email(path) for path in glob(join(args.inputs_dir, '*'))]
 
-table = Dataset(title='Benchmark', headers=['Strategy', 'Filesize', 'Duration'])
+print('\t'.join(('Strategy', 'Filesize', 'Duration')))
 
 for strategy_name, strategy in get_strategies():
     compressed_path = join(args.results_dir, 'result' + strategy.EXTENSION)
     duration = timer(lambda: strategy.compress(sample_emails, compressed_path))
 
-    table.append((strategy.EXTENSION.lstrip('.'), filesize(compressed_path), duration))
-
-print(Fore.YELLOW, table.title)
-print(Fore.GREEN, table.export('df'))
-print(Style.RESET_ALL)
+    print('\t'.join((strategy.EXTENSION.lstrip('.'), filesize(compressed_path), duration)))
